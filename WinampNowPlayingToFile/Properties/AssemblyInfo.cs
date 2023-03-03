af25502324fa5722e6a8d204f61df5c5ec068f19
Ben Hutchison <ben@aldaviva.com>
2023-03-03 04:22:43 -0800

    Increase version to 1.1.0.0

--- WinampNowPlayingToFile/Properties/AssemblyInfo.cs
+++ WinampNowPlayingToFile/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Winamp Now Playing To File")]
-[assembly: AssemblyDescription("Winamp plug-in which saves information about currently playing track to a text file")]
+[assembly: AssemblyDescription("Winamp plug-in which saves information about currently playing track to text and image files")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ben Hutchison")]
 [assembly: AssemblyProduct("Winamp Now Playing To File")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: InternalsVisibleTo("Test")]
